dccf649ee2db0e0b70dd601d0cfe25c5745ffb6d
Magson Magalhaes <magson@gmail.com>
2021-04-22 09:35:48 -0400

    Add Header and Player components

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,45 @@
+import styles from './styles.module.scss';
+
+export function Player() {
+
+
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora" />
+        <strong>Tocando agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+
+      <footer>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.emptySlider}></div>
+          <span>00:00</span>
+        </div>
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="embaralhar" />
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="anterior" />
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="play" />
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="proxima" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="repetir" />
+          </button>
+        </div>
+      </footer>
+
+    </div>
+
+  )
+}
